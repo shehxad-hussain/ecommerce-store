0449031e7cf850189255b537a94768e8970f0b72
m-shehzad455 <shehzuali5@gmail.com>
2024-10-22 23:16:37 +0500

    update dark mode with  navbar

--- src/components/Navbar/DarkMode.jsx
+++ src/components/Navbar/DarkMode.jsx
@@ -3,33 +3,37 @@ import LightButton from "../../assets/website/light-mode-button.png";
 import DarkButton from "../../assets/website/dark-mode-button.png";
 
 const DarkMode = () => {
-    const [theme, setTheme] = React.useState(localStorage.getItem("theme") ? localStorage.getItem("theme") : "light");
+  const [theme, setTheme] = React.useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
 
-    const element = document.documentElement;   //html element
+  const element = document.documentElement; // html element
 
-    React.useEffect(() => {
-        if(theme === "dark"){
-            element.classList.add("dark");
-            localStorage.setItem("theme", dark);
-        } else {
-            element.classList.remove("dark");
-            localStorage.setItem("theme", light);
+  React.useEffect(() => {
+    if (theme === "dark") {
+      element.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      element.classList.remove("dark");
+      localStorage.setItem("theme", "light");
     }
-}, [theme]);
+  }, [theme]);
 
   return (
     <div className="relative">
       <img
         src={LightButton}
-        alt="Light Mode"
-        onClick={() => setTheme(theme === "light" ? "dark" : "light")} 
-        className="w-12 cursor-pointer drop-shadow-[1px_1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 absolute right-0 z-10"
-      />
-       <img
-        src={DarkButton}
-        alt="Light Mode"
+        alt=""
         onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-        className="w-12 cursor-pointer drop-shadow-[1px_1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 absolute right-0 z-10"
+        className={`w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 absolute right-0 z-10 ${
+          theme === "dark" ? "opacity-0" : "opacity-100"
+        } `}
+      />
+      <img
+        src={DarkButton}
+        alt=""
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        className="w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300"
       />
     </div>
   );
