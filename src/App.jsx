6909d9af335682ad7e14ce40527ceacbf57b4772
m-shehzad455 <shehzuali5@gmail.com>
2024-10-23 23:29:17 +0500

    update hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 const App = () => {
   return (
-    <div><Navbar />
-
-
+    <div>
+      <Navbar />
+      <Hero />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
