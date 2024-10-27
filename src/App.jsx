169caadf373e6d167a9bfad84f206233f1bd557b
m-shehzad455 <shehzuali5@gmail.com>
2024-10-27 20:53:25 +0500

    update testimonials sections

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import "aos/dist/aos.css";
 import TopProducts from "./components/TopProducts/TopProducts";
 import Banner from "./components/Banner/Banner";
 import Subscribe from "./components/Subscribe/Subscribe";
+import Testimonials from "./components/Testimonials/Testimonials";
 
 const App = () => {
   React.useEffect(() => {
@@ -26,6 +27,7 @@ const App = () => {
       <TopProducts />
       <Banner />
       <Subscribe />
+      <Testimonials />
     </div>
   );
 };
